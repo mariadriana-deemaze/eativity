91bfef7a79e8ba3fc99e0d82ca391fe6d93aecff
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-01-09 12:13:14 +0000

    feat: configure nodemailer and send email on user sign-up

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from "@nestjs/common";
 
 import { ConfigModule } from "@nestjs/config";
 
+import { MailerModule } from "@nestjs-modules/mailer";
+
 import { PrismaModule } from "./prisma/prisma.module";
 
 import { AuthModule } from "./auth/auth.module";
@@ -12,6 +14,26 @@ import { UserModule } from "./user/user.module";
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: "localhost",
+        port: 1025,
+        ignoreTLS: true,
+        secure: false,
+      },
+      defaults: {
+        from: '"No Reply" <no-reply@localhost>',
+      },
+      preview: true, // preview e-mail in browser
+      // TODO: React e-mail <-> https://react.email/docs/integrations/nodemailer
+      /*  template: {
+        dir: __dirname + '/templates',
+        //adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      }, */
     }),
     UserModule,
     AuthModule,
