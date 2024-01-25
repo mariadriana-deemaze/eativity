ff0ee261ea9fb7c8b5b3f196b23a4588761f7347
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2024-01-25 12:06:24 +0000

    styles: dashboard skeletons

--- app/screens/protected/dashboard.tsx
+++ app/screens/protected/dashboard.tsx
@@ -10,6 +10,7 @@ import {
   FlatList,
   Badge,
   PresenceTransition,
+  Skeleton,
 } from "native-base";
 
 import { FlatListComponent, FlatListProps } from "react-native";
@@ -31,6 +32,94 @@ import { Recipe } from "../../types";
 
 import { lightTheme as theme } from "../../theme";
 
+const GreetingSkeleton = () => {
+  return (
+    <VStack space="4" px="4">
+      <VStack space="1">
+        <Skeleton
+          height="5"
+          width="32"
+          rounded="md"
+          bgColor={theme.background.secondary}
+          startColor={theme.background.primary}
+        />
+        <Skeleton
+          height="5"
+          width="48"
+          rounded="md"
+          bgColor={theme.background.secondary}
+          startColor={theme.background.primary}
+        />
+      </VStack>
+      <Skeleton
+        height="5"
+        width="72"
+        rounded="md"
+        bgColor={theme.background.secondary}
+        startColor={theme.background.primary}
+      />
+      <Skeleton
+        height="5"
+        width="64"
+        rounded="md"
+        bgColor={theme.background.secondary}
+        startColor={theme.background.primary}
+      />
+    </VStack>
+  );
+};
+
+const SliderSkeleton = () => {
+  return (
+    <VStack space="3" px="4">
+      <HStack space="3">
+        <Skeleton
+          height="40"
+          width="48"
+          rounded="md"
+          bgColor={theme.background.secondary}
+          startColor={theme.background.primary}
+        />
+        <Skeleton
+          height="40"
+          width="20"
+          rounded="md"
+          bgColor={theme.background.secondary}
+          startColor={theme.background.primary}
+        />
+        <Skeleton
+          height="40"
+          width="20"
+          rounded="md"
+          bgColor={theme.background.secondary}
+          startColor={theme.background.primary}
+        />
+      </HStack>
+      <Skeleton
+        height="5"
+        width="32"
+        rounded="md"
+        bgColor={theme.background.secondary}
+        startColor={theme.background.primary}
+      />
+      <Skeleton
+        height="5"
+        width="72"
+        rounded="md"
+        bgColor={theme.background.secondary}
+        startColor={theme.background.primary}
+      />
+      <Skeleton
+        height="5"
+        width="64"
+        rounded="md"
+        bgColor={theme.background.secondary}
+        startColor={theme.background.primary}
+      />
+    </VStack>
+  );
+};
+
 export const Dashboard = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [sliderInformationVisibility, setSliderInformationVisibility] =
@@ -43,6 +132,8 @@ export const Dashboard = () => {
   const userSliceState = useSelector((state: IRootState) => state.user);
 
   const dispatch = useAppDispatch();
+
+  const allDataLoaded = userSliceState.user;
 
   const currentRecipe = useMemo(
     () =>
@@ -70,8 +161,11 @@ export const Dashboard = () => {
 
   return (
     <VStack space="10" bg={theme.background.primary} minH="full">
+      {/**
+       * Greeting
+       */}
       <VStack space="6" mt="5">
-        {userSliceState.user ? (
+        {allDataLoaded ? (
           <VStack space="1" px="4">
             <VStack>
               <Text
@@ -108,170 +202,179 @@ export const Dashboard = () => {
             </Text>
           </VStack>
         ) : (
-          <>
-            <Text>Skeleton here</Text>
-          </>
+          <GreetingSkeleton />
         )}
 
-        <VStack space="3">
-          <FlatList
-            ref={ref}
-            horizontal
-            keyExtractor={(item) => item.id}
-            data={DUMMY_RECOMMENDED_RECIPES}
-            initialScrollIndex={currentSlide}
-            getItemLayout={(_data, index) => ({
-              length: 50,
-              offset: 50 * index,
-              index,
-            })}
-            renderItem={({ item: recipe, index }) => (
-              <Pressable
-                key={`recipe_${recipe.id}`}
-                onPress={() => {
-                  setSliderInformationVisibility(false);
-                  setCurrentSlide(index);
+        {/**
+         * Slider
+         */}
+        {allDataLoaded ? (
+          <VStack space="3">
+            <FlatList
+              ref={ref}
+              horizontal
+              keyExtractor={(item) => item.id}
+              data={DUMMY_RECOMMENDED_RECIPES}
+              initialScrollIndex={currentSlide}
+              getItemLayout={(_data, index) => ({
+                length: 50,
+                offset: 50 * index,
+                index,
+              })}
+              renderItem={({ item: recipe, index }) => (
+                <Pressable
+                  key={`recipe_${recipe.id}`}
+                  onPress={() => {
+                    setSliderInformationVisibility(false);
+                    setCurrentSlide(index);
+                  }}
+                >
+                  <Image
+                    height="40"
+                    alt={`Cover image for recipe ${recipe.name}`}
+                    width={index === currentSlide ? "48" : "20"}
+                    borderRadius="2xl"
+                    source={{
+                      uri: recipe.image,
+                    }}
+                  />
+
+                  <HStack
+                    position="absolute"
+                    bottom="2"
+                    left="2"
+                    alignSelf="center"
+                    space="2"
+                  >
+                    {index === currentSlide &&
+                      recipe.categories?.map((category) => (
+                        <Badge
+                          key={`${recipe.id}_badge_for_${category.id}`}
+                          variant="subtle"
+                        >
+                          {category.title}
+                        </Badge>
+                      ))}
+                  </HStack>
+                </Pressable>
+              )}
+              contentContainerStyle={{
+                paddingLeft: 10,
+                paddingRight: 10,
+              }}
+              ItemSeparatorComponent={() => <View style={{ width: 10 }} />}
+            />
+
+            <VStack
+              space="2"
+              px="4"
+              width="100%"
+              alignItems="flex-start"
+              minHeight="32"
+            >
+              <PresenceTransition
+                visible={sliderInformationVisibility}
+                initial={{
+                  opacity: 0,
+                }}
+                animate={{
+                  opacity: 1,
+                  translateY: 10,
+                  transition: {
+                    duration: 250,
+                  },
                 }}
               >
-                <Image
-                  height="40"
-                  alt={`Cover image for recipe ${recipe.name}`}
-                  width={index === currentSlide ? "48" : "20"}
-                  borderRadius="2xl"
-                  source={{
-                    uri: recipe.image,
-                  }}
-                />
-
-                <HStack
-                  position="absolute"
-                  bottom="2"
-                  left="2"
-                  alignSelf="center"
-                  space="2"
-                >
-                  {index === currentSlide &&
-                    recipe.categories?.map((category) => (
-                      <Badge
-                        key={`${recipe.id}_badge_for_${category.id}`}
-                        variant="subtle"
-                      >
-                        {category.title}
-                      </Badge>
-                    ))}
-                </HStack>
-              </Pressable>
-            )}
-            contentContainerStyle={{
-              paddingLeft: 10,
-              paddingRight: 10,
-            }}
-            ItemSeparatorComponent={() => <View style={{ width: 10 }} />}
-          />
-
-          <VStack
-            space="2"
-            px="4"
-            width="100%"
-            alignItems="flex-start"
-            minHeight="32"
-          >
-            <PresenceTransition
-              visible={sliderInformationVisibility}
-              initial={{
-                opacity: 0,
-              }}
-              animate={{
-                opacity: 1,
-                translateY: 10,
-                transition: {
-                  duration: 250,
-                },
-              }}
-            >
-              {currentRecipe && (
-                <VStack space="2" width="100%" alignItems="flex-start">
-                  <HStack
-                    space="3"
-                    bg={theme.background.secondary}
-                    py="1"
-                    px="2"
-                    borderRadius="full"
-                  >
-                    <HStack space="1" alignItems="center">
-                      <View
-                        bg={theme.blue.primary}
-                        height="2"
-                        width="2"
-                        borderRadius="full"
-                      />
-                      <Text
-                        fontFamily="PlusJakartaSans_200ExtraLight"
-                        color={theme.text.paragraph.secondary}
-                        fontSize="xs"
-                        isTruncated
-                      >
-                        {currentRecipe.proteins} protein
-                      </Text>
-                    </HStack>
-                    <HStack space="1" alignItems="center">
-                      <View
-                        bg={theme.red.primary}
-                        height="2"
-                        width="2"
-                        borderRadius="full"
-                      />
-                      <Text
-                        fontFamily="PlusJakartaSans_200ExtraLight"
-                        color={theme.text.paragraph.secondary}
-                        fontSize="xs"
-                        isTruncated
-                      >
-                        {currentRecipe.fats} fat
-                      </Text>
-                    </HStack>
-                    <HStack space="1" alignItems="center">
-                      <View
-                        bg={theme.purple.primary}
-                        height="2"
-                        width="2"
-                        borderRadius="full"
-                      />
-                      <Text
-                        fontFamily="PlusJakartaSans_200ExtraLight"
-                        color={theme.text.paragraph.secondary}
-                        fontSize="xs"
-                        isTruncated
-                      >
-                        {currentRecipe.carbohydrates} carbs
-                      </Text>
-                    </HStack>
-                  </HStack>
-                  <VStack space="1">
-                    <Text
-                      fontFamily="PlusJakartaSans_600SemiBold"
-                      color={theme.text.paragraph.primary}
-                      fontSize="md"
-                      isTruncated
+                {currentRecipe && (
+                  <VStack space="2" width="100%" alignItems="flex-start">
+                    <HStack
+                      space="3"
+                      bg={theme.background.secondary}
+                      py="1"
+                      px="2"
+                      borderRadius="full"
                     >
-                      {currentRecipe.name}
-                    </Text>
-                    <Text
-                      fontFamily="PlusJakartaSans_200ExtraLight"
-                      color={theme.text.paragraph.secondary}
-                      fontSize="sm"
-                      numberOfLines={2}
-                      isTruncated
-                    >
-                      {currentRecipe.description}
-                    </Text>
+                      <HStack space="1" alignItems="center">
+                        <View
+                          bg={theme.blue.primary}
+                          height="2"
+                          width="2"
+                          borderRadius="full"
+                        />
+                        <Text
+                          fontFamily="PlusJakartaSans_200ExtraLight"
+                          color={theme.text.paragraph.secondary}
+                          fontSize="xs"
+                          isTruncated
+                        >
+                          {currentRecipe.proteins} protein
+                        </Text>
+                      </HStack>
+                      <HStack space="1" alignItems="center">
+                        <View
+                          bg={theme.red.primary}
+                          height="2"
+                          width="2"
+                          borderRadius="full"
+                        />
+                        <Text
+                          fontFamily="PlusJakartaSans_200ExtraLight"
+                          color={theme.text.paragraph.secondary}
+                          fontSize="xs"
+                          isTruncated
+                        >
+                          {currentRecipe.fats} fat
+                        </Text>
+                      </HStack>
+                      <HStack space="1" alignItems="center">
+                        <View
+                          bg={theme.purple.primary}
+                          height="2"
+                          width="2"
+                          borderRadius="full"
+                        />
+                        <Text
+                          fontFamily="PlusJakartaSans_200ExtraLight"
+                          color={theme.text.paragraph.secondary}
+                          fontSize="xs"
+                          isTruncated
+                        >
+                          {currentRecipe.carbohydrates} carbs
+                        </Text>
+                      </HStack>
+                    </HStack>
+                    <VStack space="1">
+                      <Text
+                        fontFamily="PlusJakartaSans_600SemiBold"
+                        color={theme.text.paragraph.primary}
+                        fontSize="md"
+                        isTruncated
+                      >
+                        {currentRecipe.name}
+                      </Text>
+                      <Text
+                        fontFamily="PlusJakartaSans_200ExtraLight"
+                        color={theme.text.paragraph.secondary}
+                        fontSize="sm"
+                        numberOfLines={2}
+                        isTruncated
+                      >
+                        {currentRecipe.description}
+                      </Text>
+                    </VStack>
                   </VStack>
-                </VStack>
-              )}
-            </PresenceTransition>
+                )}
+              </PresenceTransition>
+            </VStack>
           </VStack>
-        </VStack>
+        ) : (
+          <SliderSkeleton />
+        )}
       </VStack>
+
+      {/**
+       * Future sections
+       */}
       <VStack space="6" px="4">
         <Text
           fontFamily="PlusJakartaSans_600SemiBold"
